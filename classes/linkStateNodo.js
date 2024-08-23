f269c22b42053f487b75c700a751845cf6c4d235
andresquez <andresequez@gmail.com>
2024-08-23 08:22:53 -0600

    feat: start flooding

--- classes/linkStateNodo.js
+++ classes/linkStateNodo.js
@@ -13,3 +13,19 @@ class Nodo {
       return `Nodo (${this.name})`;
   }
 }
+
+class LinkStateNodo extends Nodo {
+  constructor(name) {
+      super(name);
+      this.paths = new Map(); // Stores the shortest paths from this node to all other nodes
+  }
+
+  agregarVecino(neighbor, weight) {
+      this.neighbors.push({ node: neighbor, weight });
+  }
+
+  getVecinos() {
+      return this.neighbors;
+  }
+  
+}
